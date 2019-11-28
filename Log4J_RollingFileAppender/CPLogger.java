3e80309974b9d7ffd760edb4844681d1814f3a41
nangkyeong <nangkyeong.lee@gmail.com>
2019-11-29 00:08:28 +0900

    191129 comment update

--- Log4J_RollingFileAppender/CPLogger.java
+++ Log4J_RollingFileAppender/CPLogger.java
@@ -11,8 +11,9 @@ import org.apache.logging.log4j.Logger;
 
 import com.user.UserEntity;
 
-//HttpServletRequest, logger 이름을 넘겨받고 로그 파일 기록
+//HttpServletRequest, logger 이름을 넘겨받고 로그 내용을 파일로 출력 
 public class CPLogger {
+	
 	private Logger logger = null;
 	private String ip, sessionId, userid;
 
@@ -20,20 +21,25 @@ public class CPLogger {
 		logger = LogManager.getLogger(loggername);
 	}
 
-	public void GetLogging(HttpServletRequest req, Map<String,String> tolog) { // String 배열 : 추후 더 기록할 요소 존재 가능
+	public void GetLogging(HttpServletRequest req, Map<String,String> tolog) { 
+		
+		// 해당 요청의 ip 
 		ip = req.getHeader("X-FORWARDED-FOR");
 		if (ip == null)
 			ip = req.getRemoteAddr();
+		
+		// 세션 id
 		sessionId = req.getRequestedSessionId();
+		
+		// 로그인하지 않은 경우 "nonuser"로 처리 
 		if (req.getSession().getAttribute("userinfo") == null)
 			userid = req.getRequestedSessionId();
 		else
 			userid = ((UserEntity) req.getSession().getAttribute("userinfo")).getId();
 		
-		//key 값 얻기 
+		// 로깅할 내용 추출해내고 로그 메세지 생성 
 		Iterator<String> itr = tolog.keySet().iterator();
 		String tolog_key = itr.next();
-		
 		logger.info(" \"ip\": \"{}\", \"sessionId\": \"{}\", \"userid\": \"{}\", \""+tolog_key+"\" : \"{}\"", ip,
 				sessionId, userid, tolog.get(tolog_key));
 	}
